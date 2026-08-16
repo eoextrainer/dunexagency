8f9ff74aeddfa91bd9dd56b709f415d143ca932d
Sosthene Grosset-Janin <educ1.eoex@gmail.com>
2026-08-16 19:36:40 +0200

    Fix Pages base path for eoexagency

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dunexagency/',
+  base: '/eoexagency/',
   server: {
     port: 3000,
     host: '0.0.0.0',
